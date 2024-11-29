52336d1427c794bc9d2a577e8aefc7dfe04af9c9
Syed Momin Naqvi <32732835+syedmominnaqvi@users.noreply.github.com>
2024-11-29 22:20:14 +0500

    Update WebConfig.java

--- config/WebConfig.java
+++ config/WebConfig.java
@@ -1,3 +1,5 @@
+package com.iba.course_mgmt.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
